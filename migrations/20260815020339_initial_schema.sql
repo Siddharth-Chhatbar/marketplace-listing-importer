1f6ba6ced7a900fe575aa068344a5228a360aaff
Siddharth-Chhatbar <siddharth3901@gmail.com>
2026-08-15 11:14:21 +0900

    goose init

---
+++ migrations/20260815020339_initial_schema.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+CREATE TABLE delivery_requests(
+  id UUID PRIMARY KEY,
+  recepient TEXT NOT NULL,
+  subject TEXT NOT NULL,
+  body TEXT NOT NULL,
+  status TEXT NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+-- +goose Down
+DROP TABLE delivery_requests;
